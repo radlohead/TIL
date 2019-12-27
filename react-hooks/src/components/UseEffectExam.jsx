5dc26895f3b6e055953360a5e7418f0b21112d6b
= <radlohead@naver.com>
2019-12-27 12:47:25 +0900

    react-hooks - useEffect 설명 추가

--- react-hooks/src/components/UseEffectExam.jsx
+++ react-hooks/src/components/UseEffectExam.jsx
@@ -5,9 +5,17 @@ export const UseEffectExam = () => {
 
     // useEffect는 componentDidMount, componentDidUpdate와 비슷합니다
     useEffect(() => {
-        if (count > 3) return
+        if (count > 3) return // 조건문을 hooks 내부에 삽입할 수 있습니다.
         document.title = count
     })
+
+    // 조건문 내부에서 hooks을 사용하면 사이드 이펙트가 발생할 수 있습니다.
+    // hooks은 항상 최상위에 위치해야 합니다.
+    // if(!count){
+    //     useEffect(() => {
+    //         document.title = count
+    //     })
+    // }
 
     return (
         <div>
