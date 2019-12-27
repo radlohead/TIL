ee0062a3bde3467763170913f2b87b8b714518e2
= <radlohead@naver.com>
2019-12-27 16:46:26 +0900

    react-hooks - useReducer reset

--- react-hooks/src/components/UseReducerExam.jsx
+++ react-hooks/src/components/UseReducerExam.jsx
@@ -1,11 +1,16 @@
 import React, { useReducer } from 'react'
 
+const init = initialCount => {
+    return { value: initialCount }
+}
 const reducer = (state, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return { value: state.value + 1 }
         case 'DECREMENT':
             return { value: state.value - 1 }
+        case 'RESET':
+            return init(action.payload)
         default:
             return state
     }
@@ -16,6 +21,9 @@ const UserReducerExam = () => {
     return (
         <div>
             <p>현재 카운터 값은 {state.value} 입니다.</p>
+            <button onClick={() => dispatch({ type: 'RESET', payload: 0 })}>
+                reset
+            </button>
             <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
             <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
         </div>
