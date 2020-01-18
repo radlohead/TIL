98512d7415b3d78a78a72679d7b8eb11e459ad22
= <radlohead@naver.com>
2020-01-18 16:33:41 +0900

    TDD 발표 - List async, await, axios, promise

--- TDD/vue-jest2/src/components/List.vue
+++ TDD/vue-jest2/src/components/List.vue
@@ -7,11 +7,39 @@
 </template>
 
 <script>
+import fetch from "node-fetch";
+import axios from "axios";
+
 export default {
   data() {
     return {
       list: [1, 2, 3]
     };
+  },
+  created() {},
+  methods: {
+    async fetchPost(postNum) {
+      const postResponse = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${postNum}`
+      );
+      const post = await postResponse.json();
+      return post;
+    },
+    async fetchPost2(postNum) {
+      const postResponse = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts/${postNum}`
+      );
+      if (postResponse.status !== 200) {
+        console.error("fetchPost2 status error");
+        return;
+      }
+      return postResponse.data;
+    },
+    fetchPost3(isState) {
+      return new Promise((resolve, reject) => {
+        return resolve(isState);
+      });
+    }
   }
 };
 </script>
--- TDD/vue-jest2/test/unit/specs/List.spec.js
+++ TDD/vue-jest2/test/unit/specs/List.spec.js
@@ -2,12 +2,34 @@ import { shallowMount } from "@vue/test-utils";
 import List from "@/components/List";
 
 describe("List.vue", () => {
-  it("list data", () => {
-    const wrapper = shallowMount(List);
+  const wrapper = shallowMount(List);
+
+  test("list data", () => {
     expect(wrapper.vm.list).toEqual([1, 2, 3]);
   });
-  it("list length", () => {
-    const wrapper = shallowMount(List);
+  test("list length", () => {
     expect(wrapper.findAll("li")).toHaveLength(3);
+  });
+  test("list fetchPost", async () => {
+    const postNum = 1;
+    const response = await wrapper.vm.fetchPost(postNum);
+    expect(response).toHaveProperty("id", postNum);
+  });
+  test("list fetchPost2", async () => {
+    const postNum = 2;
+    const response = await wrapper.vm.fetchPost2(postNum);
+    expect(response).toHaveProperty("id", postNum);
+  });
+  test("list fetchPost3", () => {
+    const isState = true;
+    const response = wrapper.vm.fetchPost3(isState);
+    response.then(res => {
+      expect(res).toBeTruthy();
+    });
+  });
+  test("list fetchPost3", async () => {
+    const isState = false;
+    const response = await wrapper.vm.fetchPost3(isState);
+    expect(response).toBeFalsy();
   });
 });
