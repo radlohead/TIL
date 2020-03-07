9844b977dab447f5b2a4b0355b5ba3c615b49a79
radlohead <radlohead@naver.com>
2020-03-07 11:17:37 +0900

    electron/example1 - basic code

---
+++ electron/example1/main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
+
+// 전역객체로 선언
+let win
+
+function createWindow() {
+    // 브라우저 창을 만듬
+    win = new BrowserWindow({ width: 800, height: 600 })
+    // index.html을 로드
+    win.loadURL(
+        url.format({
+            pathname: path.join(__dirname, 'index.html'),
+            protocol: 'file:',
+            slashes: true
+        })
+    )
+
+    // 개발자도구 오픈
+    win.webContents.openDevTools()
+
+    // 윈도우가 닫힐 때 발생하는 이벤트
+    win.on('closed', () => {
+        win = null
+    })
+}
+
+// 사용 준비가 완료되면 윈도우를 오픈
+app.on('ready', createWindow)
+
+// 모든 창이 닫히면 종료
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+app.on('activate', () => {
+    // mac에서 독 아이콘을 클릭하면 오픈
+    if (win === null) {
+        createWindow()
+    }
+})
