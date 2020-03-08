f9f09e4224f8863e9d05828f880e022cd2313569
radlohead <radlohead@naver.com>
2020-03-08 18:15:01 +0900

    electron/notifications - 알림 테스트

---
+++ electron/notifications/main.js
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: { nodeIntegration: true }
+    })
+    mainWindow.loadURL('file://' + __dirname + '/index.html')
+})
