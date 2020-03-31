976a05996bcca23231fbba43d98af2cc3b734e9e
radlohead <radlohead@naver.com>
2020-03-31 22:48:24 +0900

    vue-end - axios.interceptors 적용

--- vue-end/vue-til/src/api/index.js
+++ vue-end/vue-til/src/api/index.js
@@ -2,8 +2,21 @@ import axios from 'axios'
 
 function createInstance() {
     const instance = axios.create({
-        baseURL: process.env.VUE_APP_API_URL
+        baseURL: process.env.VUE_APP_API_URL,
+        timeout: 3000
     })
+    return setInterceptors(instance)
+}
+function setInterceptors(instance) {
+    instance.interceptors.request.use(
+        function(config) {
+            config.headers.CustomHeader = 'foobar'
+            return config
+        },
+        function(error) {
+            return Promise.reject(error)
+        }
+    )
     return instance
 }
 
