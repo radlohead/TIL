7ea84a7b0d090854f3d79eb0765c5fc161c1a3a0
radlohead <radlohead@naver.com>
2020-03-31 21:42:06 +0900

    vue-end - 코드 정리

--- vue-end/vue-til/src/App.vue
+++ vue-end/vue-til/src/App.vue
@@ -1,31 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div id="app">
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+    import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: "App",
-  created() {
-    var a = 10;
-  },
-  components: {
-    HelloWorld
-  }
-};
+    export default {
+        name: 'App',
+        created() {},
+        components: {
+            HelloWorld
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
