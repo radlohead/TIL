b348ea0fb8a051094880ee10688a3cbc96d98add
radlohead <radlohead@naver.com>
2020-04-02 22:23:39 +0900

    refactoring - ch1

--- vue-end/vue-til/src/components/HelloWorld.vue
+++ vue-end/vue-til/src/components/HelloWorld.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="hello">
         <h1 class="title">{{ msg }}</h1>
+        <div id="app">
+            <div v-for="item in list" :key="item">
+                <p :class="bindId(item)">{{ item }}</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,6 +14,25 @@
         name: 'HelloWorld',
         props: {
             msg: String
+        },
+        data() {
+            return {
+                list: []
+            }
+        },
+        created() {
+            for (var i = 0; i < 3; i++) {
+                this._data.list.push(i)
+            }
+            this.$nextTick(() => {
+                var dom = document.querySelector('.item-0')
+                dom.style.backgroundColor = 'red'
+            })
+        },
+        methods: {
+            bindId: function(item) {
+                return 'item-' + item
+            }
         }
     }
 </script>
