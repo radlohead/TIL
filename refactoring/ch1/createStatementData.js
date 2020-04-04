7e11391dffff562d1fa1bcdbf5052115cedfa43f
radlohead <radlohead@naver.com>
2020-04-04 11:55:34 +0900

    refactoring - ch1 refactoring 14

--- refactoring/ch1/createStatementData.js
+++ refactoring/ch1/createStatementData.js
@@ -1,28 +1,20 @@
-class PerormanceCalculator {
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case 'tragedy':
+            return new TragedyCalculator(aPerformance, aPlay)
+        case 'comedy':
+            return new ComedyCalculator(aPerformance, aPlay)
+        default:
+            throw new Error(`알 수 없는 장르: ${aPlay.type}`)
+    }
+}
+class PerformanceCalculator {
     constructor(aPerformance, aPlay) {
         this.performances = aPerformance
         this.play = aPlay
     }
     get amount() {
-        let result = 0
-
-        switch (this.play.type) {
-            case 'tragedy': //비극
-                result = 40000
-                if (this.performances.audience > 30) {
-                    result += 1000 * (this.performances.audience - 30)
-                }
-                break
-            case 'comedy': //희극
-                result = 30000
-                if (this.performances.audience > 20) {
-                    result += 1000 + 500 * (this.performances.audience - 20)
-                }
-                break
-            default:
-                throw new Error(`알 수 없는 장르: ${this.play.type}`)
-        }
-        return result
+        throw new Error('하위 클래스에서 처리')
     }
     get volumeCredits() {
         let volumeCredits = 0
@@ -31,6 +23,24 @@ class PerormanceCalculator {
             volumeCredits += Math.floor(this.performances.audience / 5)
         }
         return volumeCredits
+    }
+}
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000
+        if (this.performances.audience > 30) {
+            result += 1000 * (this.performances.audience - 30)
+        }
+        return result
+    }
+}
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000
+        if (this.performances.audience > 20) {
+            result += 1000 + 500 * (this.performances.audience - 20)
+        }
+        return result
     }
 }
 
@@ -43,7 +53,7 @@ function createStatementData(invoice, plays) {
     return statementData
 
     function enrichPerformance(aPerformance) {
-        const calculator = new PerormanceCalculator(
+        const calculator = createPerformanceCalculator(
             aPerformance,
             playFor(aPerformance)
         )
