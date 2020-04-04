15ae883946efd8387ca795d16d7168a98e937e9b
radlohead <radlohead@naver.com>
2020-04-04 09:55:13 +0900

    refactoring - ch1 테스트 코드 추가

--- refactoring/ch1/app.js
+++ refactoring/ch1/app.js
@@ -12,7 +12,7 @@ function renderPlainText(data, plays) {
     }
     result += `총액: ${usd(data.totalAmount)}\n`
     result += `적립 포인트: ${data.totalVolumeCredits}점\n`
-    console.log(result)
+    // console.log(result)
     return result
 
     function usd(aNumber) {
@@ -86,3 +86,28 @@ const statement = (invoice, plays) => {
     }
 }
 statement(invoicesJSON[0], playsJSON)
+
+function test() {
+    const t1 =
+        statement(invoicesJSON[0], playsJSON).indexOf(
+            `청구 내역 (고객명: BigCo)`
+        ) > -1
+    const t2 =
+        statement(invoicesJSON[0], playsJSON).indexOf(
+            `Hamlet: $650.00 (55석)`
+        ) > -1
+    const t3 =
+        statement(invoicesJSON[0], playsJSON).indexOf(
+            `As You Like It: $385.00 (35석)`
+        ) > -1
+    const t4 =
+        statement(invoicesJSON[0], playsJSON).indexOf(
+            `Othello: $500.00 (40석)`
+        ) > -1
+    const t5 =
+        statement(invoicesJSON[0], playsJSON).indexOf(`총액: $1,535.00`) > -1
+    const t6 =
+        statement(invoicesJSON[0], playsJSON).indexOf(`적립 포인트: 47점`) > -1
+    return [t1, t2, t3, t4, t5, t6].every(state => state)
+}
+console.log(test())
