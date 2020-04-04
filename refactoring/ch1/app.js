7b5216dea203f704dcb1e652783fe36a68a15377
radlohead <radlohead@naver.com>
2020-04-04 10:52:35 +0900

    refactoring - ch1 refactoring 11

--- refactoring/ch1/app.js
+++ refactoring/ch1/app.js
@@ -2,7 +2,14 @@ const invoicesJSON = require('./invoices.json')
 const playsJSON = require('./plays.json')
 const { createStatementData } = require('./createStatementData.js')
 
-function renderPlainText(data, plays) {
+function usd(aNumber) {
+    return new Intl.NumberFormat('en-us', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    }).format(aNumber / 100)
+}
+function renderPlainText(data) {
     let result = `청구 내역 (고객명: ${data.customer})\n`
 
     for (let perf of data.performances) {
@@ -15,20 +22,40 @@ function renderPlainText(data, plays) {
     result += `적립 포인트: ${data.totalVolumeCredits}점\n`
     // console.log(result)
     return result
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat('en-us', {
-            style: 'currency',
-            currency: 'USD',
-            minimumFractionDigits: 2
-        }).format(aNumber / 100)
-    }
 }
-
 function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays))
 }
+function htmlStatement(invoice, plays) {
+    return renderHTML(createStatementData(invoice, plays))
+}
+function renderHTML(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`
+    result += `<table>
+        <tr>
+            <th>연극</th>
+            <th>좌석 수</th>
+            <th>금액</th>
+        </tr>
+    `
+    for (let perf of data.performances) {
+        result += `
+            <tr>
+                <td>${perf.play.name}</td>
+                <td>${perf.audience}</td>
+                <td>${usd(perf.amount)}</td>
+            </tr>\n
+        `
+    }
+    result += `
+        </table>\n
+        <p>총액: <em>${usd(data.totalAmount)}</em></p>\n
+        <p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n
+    `
+    return result
+}
 statement(invoicesJSON[0], playsJSON)
+console.log(htmlStatement(invoicesJSON[0], playsJSON))
 
 function test() {
     const t1 =
