6c1f4397fa0d55962f7f046491ea0c6f827a85c8
radlohead <radlohead@naver.com>
2020-04-07 22:46:00 +0900

    refactoring - extract-function/ch2 지역변수의 값을 변경할때

--- refactoring/extract-function/ch2/step1.js
+++ refactoring/extract-function/ch2/step1.js
@@ -1,17 +1,17 @@
 // 지역 변수를 사용할 때
 function printBanner() {}
 function printOwing(invoice) {
-    let outstanding = 0
-
     printBanner()
-
-    // 미해결 채무(outstanding)를 계산한다.
+    const outstanding = calculateOutstanding(invoice)
+    recordDueDate(invoice)
+    printDetails(invoice, outstanding)
+}
+function calculateOutstanding(invoice) {
+    let outstanding = 0
     for (const o of invoice.orders) {
         outstanding += o.amount
     }
-
-    recordDueDate(invoice)
-    printDetails(invoice, outstanding)
+    return outstanding
 }
 function recordDueDate(invoice) {
     const today = new Date()
