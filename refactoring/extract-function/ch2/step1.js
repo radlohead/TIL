3cc456248b65ad51c9f5f1aabb253f2cf079b2c0
radlohead <radlohead@naver.com>
2020-04-07 22:35:09 +0900

    refactoring - extract-function/ch2 인라인코드 함수화

--- refactoring/extract-function/ch2/step1.js
+++ refactoring/extract-function/ch2/step1.js
@@ -10,14 +10,18 @@ function printOwing(invoice) {
         outstanding += o.amount
     }
 
+    recordDueDate(invoice)
+    printDetails(invoice, outstanding)
+}
+function recordDueDate(invoice) {
     const today = new Date()
     invoice.dueDate = new Date(
         today.getFullYear(),
         today.getMonth(),
         today.getDate() + 30
     )
-
-    // 세부 사항을 출력한다.
+}
+function printDetails(invoice, outstanding) {
     console.log(`고객명: ${invoice.customer}`)
     console.log(`채무액: ${outstanding}`)
     console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`)
