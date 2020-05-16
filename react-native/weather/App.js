52e6f8220c97773522e7b7e4917c7ed77fe04407
radlohead <radlohead@naver.com>
2020-05-16 12:50:10 +0900

    react-native - step3

--- react-native/weather/App.js
+++ react-native/weather/App.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 class HelloWorldApp extends Component {
   render() {
     return (
-      <View style={{padding: 40}}>
+      <View style={styles.container}>
         <Text style={styles.title}>Hello world!</Text>
       </View>
     );
@@ -12,6 +12,11 @@ class HelloWorldApp extends Component {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   title: {
     color: 'red',
     fontSize: 30,
