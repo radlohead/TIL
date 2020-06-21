90a484458a1831c94919866adf68825d6b07b128
radlohead <radlohead@naver.com>
2020-06-21 20:12:41 +0900

    ssr - nuxt setting

---
+++ ssr/example1/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hello world!</h1>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  created() {
+    console.log(111);
+    this.init();
+  },
+  methods: {
+    async init() {
+      const response = await axios({
+        url: "https://jsonplaceholder.typicode.com/posts/1",
+        method: "GET"
+      });
+
+      console.log(22, response);
+    }
+  }
+};
+</script>
