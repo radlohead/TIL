e0bf4b99aa4c0c847496f42dffbcb68dc65364b4
radlohead <radlohead@naver.com>
2020-10-31 13:45:22 +0900

    electron/ipcRenderer - ipcRenderer 'send', 'on'

--- electron/ipcRenderer/app.js
+++ electron/ipcRenderer/app.js
@@ -1,1 +1,17 @@
-console.log('app.js')
+const { ipcRenderer } = require('electron')
+
+ipcRenderer.on('PRINT_TEXT', (_e, text) => {
+    try {
+        alert(text)
+        console.log('app text: ', text)
+    } catch (e) {
+        console.log('app error: ', e)
+    }
+})
+
+function handleClickRequestEvent() {
+    ipcRenderer.send('REQUEST_EVENT', 'request_event')
+}
+
+const $requestEventBtn = document.querySelector('#requestEventBtn')
+$requestEventBtn.addEventListener('click', () => handleClickRequestEvent())
--- electron/ipcRenderer/main.js
+++ electron/ipcRenderer/main.js
@@ -1,15 +1,24 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 
 let win
 
-app.on('ready', () => {
+function createWindow() {
     win = new BrowserWindow({
         width: 800,
         height: 600,
-        webPreferences:{
-            nodeIntegration: true
-        }
+        webPreferences: {
+            nodeIntegration: true,
+        },
     })
     win.loadURL(`file://${__dirname}/index.html`)
     win.webContents.openDevTools()
+
+    ipcMain.on('REQUEST_EVENT', (_e, eventName) => {
+        win.webContents.send('PRINT_TEXT', 'send_text')
+        console.log('main: ', eventName)
+    })
+}
+
+app.on('ready', () => {
+    createWindow()
 })
