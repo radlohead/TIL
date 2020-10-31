5b7c4da5efd5b3f54bbc716b629fd5406c8dc19b
radlohead <radlohead@naver.com>
2020-10-31 14:37:54 +0900

    electron/ipcRenderer - 기본 event 테스트

--- electron/ipcRenderer/app.js
+++ electron/ipcRenderer/app.js
@@ -2,16 +2,15 @@ const { ipcRenderer } = require('electron')
 
 ipcRenderer.on('PRINT_TEXT', (_e, text) => {
     try {
-        alert(text)
         console.log('app text: ', text)
     } catch (e) {
         console.log('app error: ', e)
     }
 })
 
+const $requestEventBtn = document.querySelector('#requestEventBtn')
+$requestEventBtn.addEventListener('click', () => handleClickRequestEvent())
+
 function handleClickRequestEvent() {
     ipcRenderer.send('REQUEST_EVENT', 'request_event')
 }
-
-const $requestEventBtn = document.querySelector('#requestEventBtn')
-$requestEventBtn.addEventListener('click', () => handleClickRequestEvent())
--- electron/ipcRenderer/main.js
+++ electron/ipcRenderer/main.js
@@ -14,8 +14,18 @@ function createWindow() {
     win.webContents.openDevTools()
 
     ipcMain.on('REQUEST_EVENT', (_e, eventName) => {
-        win.webContents.send('PRINT_TEXT', 'send_text')
+        win.webContents.send('PRINT_TEXT', 'request_event send_text!!')
         console.log('main: ', eventName)
+    })
+
+    win.webContents.on('did-finish-load', () => {
+        console.log('did-finish-load!!')
+    })
+    win.webContents.on('did-fail-load', () => {
+        console.log('did-fail-load')
+    })
+    win.webContents.on('devtools-opened', () => {
+        console.log('devtools-opened')
     })
 }
 
