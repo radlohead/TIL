c4aea5adb13677d370900c0d42ca7ae6774b4ac0
radlohead <radlohead@naver.com>
2020-10-31 15:12:12 +0900

    electron/ipcRenderer - 새로고침 외 event 테스트

--- electron/ipcRenderer/app.js
+++ electron/ipcRenderer/app.js
@@ -8,9 +8,24 @@ ipcRenderer.on('PRINT_TEXT', (_e, text) => {
     }
 })
 
-const $requestEventBtn = document.querySelector('#requestEventBtn')
-$requestEventBtn.addEventListener('click', () => handleClickRequestEvent())
+ipcRenderer.on('RELOAD_RESPONSE', (e, text) => {
+    try {
+        console.log('reload text: ', text)
+    } catch (e) {
+        console.log('reload error: ', e)
+    }
+})
 
-function handleClickRequestEvent() {
+const $requestEventBtn = document.querySelector('#requestEventBtn')
+$requestEventBtn.addEventListener('click', handleClickRequest)
+
+const $reloadBtn = document.querySelector('#reloadBtn')
+$reloadBtn.addEventListener('click', handleClickReload)
+
+function handleClickRequest() {
     ipcRenderer.send('REQUEST_EVENT', 'request_event')
+}
+
+function handleClickReload() {
+    ipcRenderer.send('RELOAD_EVENT', 'reload_event')
 }
--- electron/ipcRenderer/main.js
+++ electron/ipcRenderer/main.js
@@ -13,19 +13,37 @@ function createWindow() {
     win.loadURL(`file://${__dirname}/index.html`)
     win.webContents.openDevTools()
 
-    ipcMain.on('REQUEST_EVENT', (_e, eventName) => {
+    ipcMain.on('REQUEST_EVENT', (e, eventName) => {
         win.webContents.send('PRINT_TEXT', 'request_event send_text!!')
         console.log('main: ', eventName)
     })
 
+    ipcMain.on('RELOAD_EVENT', (e, eventName) => {
+        win.webContents.send('RELOAD_RESPONSE', 'reload_response!!')
+        win.webContents.reload()
+        console.log('main: ', eventName)
+    })
+
     win.webContents.on('did-finish-load', () => {
-        console.log('did-finish-load!!')
+        console.log('did-finish-load')
     })
     win.webContents.on('did-fail-load', () => {
         console.log('did-fail-load')
     })
     win.webContents.on('devtools-opened', () => {
         console.log('devtools-opened')
+    })
+    win.webContents.on('dom-ready', () => {
+        console.log('dom-ready')
+
+        const getURL = win.webContents.getURL()
+        const getTitle = win.webContents.getTitle()
+        console.log('getURL: ', getURL)
+        console.log('getTitle: ', getTitle)
+    })
+    win.webContents.on('close', () => {
+        console.log('close')
+        console.log('isDestroyed: ', win.webContents.isDestroyed())
     })
 }
 
