bf5b6acf772113e8147d0b8f02ac49c7add31284
radlohead <radlohead@naver.com>
2020-11-21 12:29:59 +0900

    gsap/example1 - 알파벳 샘플

--- gsap/example1/src/js/index.js
+++ gsap/example1/src/js/index.js
@@ -1,18 +1,15 @@
 import '../css/style.scss'
 
-class Square {
-    constructor() {
-        this.root = document.querySelector('#root')
-    }
-    render() {
-        this.root.innerHTML += `
-            <h1 class="title">gsap example</h1>
-        `
-    }
-    task() {
-        this.render()
-    }
-}
+MorphSVGPlugin.convertToPath('circle, rect, polygon')
 
-const test = new Square()
-test.task()
+const tl = gsap.timeline({
+    repeat: 20,
+    repeatDelay: 0.3,
+    yoyo: true,
+    defaults: { duration: 1 },
+})
+
+tl.to('#triangle', { morphSVG: '#a' }, '+=0.5')
+    .to('#square', { morphSVG: '#b' })
+    .to('#circle', { morphSVG: '#c' })
+    .timeScale(3)
