fb8abc66efa300579bf02949300e43f735f3f42b
radlohead <radlohead@naver.com>
2020-12-31 11:31:50 +0900

    add

--- react-typescript/todo/src/index.tsx
+++ react-typescript/todo/src/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import App from './components/App'
 import reportWebVitals from './reportWebVitals'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { rootReducer } from './store'
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById('root')
 )
 
