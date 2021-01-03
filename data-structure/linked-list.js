5907f16e9322ba945277563f1265a8e7b8c2f4b6
radlohead <radlohead@naver.com>
2021-01-03 21:34:42 +0900

    modify linked-list

--- data-structure/linked-list.js
+++ data-structure/linked-list.js
@@ -1,41 +1,66 @@
-class List {
-    constructor() {
-        this.list = []
-    }
-    append(data) {
-        this.list.push(data)
-    }
-    removeIndex(index) {
-        this.list.splice(index, 1)
-    }
-    indexOf(data) {
-        const targetData = this.list[data]
-        return targetData ? targetData : -1
-    }
-    insert(index, data) {
-        this.list.splice(index, 0, data)
-    }
-    isEmpty() {
-        return this.list.length > 0 ? true : false
-    }
-    size() {
-        return this.list.length
+class Node {
+    constructor(data) {
+        this.data = data
+        this.next = null
     }
 }
-// append(데이터): 리스트의 맨 끝에 데이터를 추가한다.
-// removeIndex(위치): 해당 위치에 있는 데이터를 삭제한다.
-// indexOf(데이터): 해당 데이터의 인덱스를 반환한다. 존재하지 않을 경우 결과 값은 -1이다.
-// remove(데이터): 데이터를 삭제한다.
-// insert(위치, 데이터): 해당 위치에 데이터를 삽입한다.
-// isEmpty(): 데이터가 하나도 없다면 true를, 그 외엔 false를 반환한다.
-// size(): 데이터 개수를 반환한다. 배열의 length 프로퍼티와 비슷하다.
-const list = new List()
-list.append(1) // [1]
-list.append(2) // [1,2]
-list.append(3) //[1,2,3]
-list.append(4) //[1,2,3,4]
-list.removeIndex(2) //[1,2,4]
-list.indexOf(2) //4
-list.insert(2, 3) //[1,2,3,4]
-list.isEmpty() //true
-list.size() //4
+class LinkedList {
+    constructor() {
+        this.length = 0
+        this.head = null
+    }
+    add(value) {
+        const node = new Node(value)
+        let current = this.head
+        if (!current) {
+            this.head = node
+            ++this.length
+        } else {
+            while (current.next) {
+                current = current.next
+            }
+            current.next = node
+            ++this.length
+            return node
+        }
+    }
+    search(position) {
+        let current = this.head
+        let count = 0
+        while (count < position) {
+            current = current.next
+            ++count
+        }
+        return current
+    }
+    remove(position) {
+        let current = this.head
+        let before
+        let remove
+        let count = 0
+        if (position === 0) {
+            remove = this.head
+            this.head = this.head.next
+            --this.length
+            return remove
+        } else {
+            while (count < position) {
+                before = current
+                ++count
+                current = current.next
+            }
+            remove = current
+            before.next = remove.next
+            --this.length
+            return remove
+        }
+    }
+}
+
+const list = new LinkedList()
+list.add(1)
+list.add(2)
+list.add(3)
+list.remove(1)
+console.log(list.search(1))
+console.log(list)
